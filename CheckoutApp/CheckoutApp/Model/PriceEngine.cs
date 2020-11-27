7a0f7ae66c91a85eddc8ed925f36defc70c5b9f4
Craig <craigjwestall@gmail.com>
2020-11-27 20:04:47 +0000

    Fixing a couple of bugs with the price generation.

--- CheckoutApp/CheckoutApp/Model/PriceEngine.cs
+++ CheckoutApp/CheckoutApp/Model/PriceEngine.cs
@@ -15,31 +15,46 @@ namespace CheckoutApp.Model
             var promotionItems = items.GroupBy(x => x.Discount);
 
 
-            foreach (var item in promotionItems)
+            foreach (var item in promotionItems.Where(x=>x.Key != null))
             {
                 var discountType = item.First().Discount;
 
                 if (discountType.Contains("for"))
                 {
-                    // we disocunt if we have a set amount of items
-                    var quantityRequired = int.Parse(discountType.Split(" ")[0].Trim());
-                    var price = decimal.Parse(discountType.Split(" ")[2].Trim());
-
-                    if (item.Count() >= quantityRequired)
-                    {
-                        var individualPrice = item.First().Price;
-                        total += ((item.Count() / quantityRequired) * price) + ((item.Count() % quantityRequired)* individualPrice);
-                    }
+                    total = CalculateForDiscount(total, item, discountType);
                 }
                 else if (discountType.Contains("off"))
                 {
-                    // we discount all items
-                    var discount = decimal.Parse(discountType.Split(" ")[0].Trim());
-                    var quantityRequired = int.Parse(discountType.Split(" ")[2].Trim());
-
-
+                    CalculateOffDiscount(discountType);
 
                 }
+            }
+
+            foreach(var nonPromoItem in items.Where(x=>x.Discount == null || x.Discount == ""))
+            {
+                total += nonPromoItem.Price;
+            }
+
+            return total;
+        }
+
+        private static void CalculateOffDiscount(string discountType)
+        {
+            // we discount all items
+            var discount = decimal.Parse(discountType.Split(" ")[0].Trim());
+            var quantityRequired = int.Parse(discountType.Split(" ")[2].Trim());
+        }
+
+        private static decimal CalculateForDiscount(decimal total, IGrouping<string, Item> item, string discountType)
+        {
+            // we disocunt if we have a set amount of items
+            var quantityRequired = int.Parse(discountType.Split(" ")[0].Trim());
+            var price = decimal.Parse(discountType.Split(" ")[2].Trim());
+
+            if (item.Count() >= quantityRequired)
+            {
+                var individualPrice = item.First().Price;
+                total += ((item.Count() / quantityRequired) * price) + ((item.Count() % quantityRequired) * individualPrice);
             }
 
             return total;
--- CheckoutApp/CheckoutAppTests/BasketTests.cs
+++ CheckoutApp/CheckoutAppTests/BasketTests.cs
@@ -55,5 +55,57 @@ namespace CheckoutAppTests
 
             Assert.AreEqual(40, basket.CalculatePrice());
         }
+
+        [TestMethod]
+        public void TestMultipleDiscountOfBasket2()
+        {
+            var basket = Basket.Instance();
+            var itemToAdd = new Item("B", 15, "3 for 40");
+            var itemToAdd2 = new Item("B", 15, "3 for 40");
+            var itemToAdd3 = new Item("B", 15, "3 for 40");
+
+            var itemToAdd4 = new Item("C", 12, "3 for 35");
+            var itemToAdd5 = new Item("C", 12, "3 for 35");
+            var itemToAdd6 = new Item("C", 12, "3 for 35");
+
+            basket.AddItem(itemToAdd);
+            basket.AddItem(itemToAdd2);
+            basket.AddItem(itemToAdd3);
+
+            basket.AddItem(itemToAdd4);
+            basket.AddItem(itemToAdd5);
+            basket.AddItem(itemToAdd6);
+
+            Assert.AreEqual(75, basket.CalculatePrice());
+        }
+
+        [TestMethod]
+        public void TestMultipleDiscountOfBasketWithSingleItem()
+        {
+            var basket = Basket.Instance();
+            var itemToAdd = new Item("B", 15, "3 for 40");
+            var itemToAdd2 = new Item("B", 15, "3 for 40");
+            var itemToAdd3 = new Item("B", 15, "3 for 40");
+
+            var itemToAdd4 = new Item("C", 12, "3 for 35");
+            var itemToAdd5 = new Item("C", 12, "3 for 35");
+            var itemToAdd6 = new Item("C", 12, "3 for 35");
+
+
+            var itemToAdd7 = new Item("A", 10, "");
+
+            basket.AddItem(itemToAdd);
+            basket.AddItem(itemToAdd2);
+            basket.AddItem(itemToAdd3);
+
+            basket.AddItem(itemToAdd4);
+            basket.AddItem(itemToAdd5);
+            basket.AddItem(itemToAdd6);
+
+            basket.AddItem(itemToAdd7);
+
+
+            Assert.AreEqual(85, basket.CalculatePrice());
+        }
     }
 }
